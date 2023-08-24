d3750dd92068bb0abaef23717103c9f7a3e6d769
MPLectures <microprogramerszoom@gmail.com>
2023-08-24 23:18:09 +0500

    10 increment/decrement operators

---
+++ bin/Basics/increment_decrement.dart
@@ -0,0 +1,27 @@
+// increment add something new
+// decrement remove something
+
+
+void main(){
+
+  int a = 10;
+  int b = 11;
+
+  --b;
+  a = a+b;
+
+  print(b--);
+  print(a+b);
+
+
+
+
+  // print(a++);
+  //
+  // print(a);
+  //
+  // a++;
+  // print(a++);
+  // print(a);
+
+}
